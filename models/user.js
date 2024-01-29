8e09ff288f1a8b55249f8cc6e38f969c991706f3
Alexey Karpov <leoxandro@yandex.ru>
2024-01-29 23:47:03 +0300

    minor changes

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
+const { UnauthorizedError } = require('../utils/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,5 +25,22 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+// eslint-disable-next-line
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Incorrect email or password'));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new UnauthorizedError('Incorrect email or password'));
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
