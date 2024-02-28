b3fd8c14e009c05ca010a02f1cf71bd17cdb04ce
Alexey Karpov <leoxandro@yandex.ru>
2024-02-28 10:23:02 +0300

    tests

--- helpers/limiter.js
+++ helpers/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 1440 * 60 * 1000,
-  max: 500,
+  max: 50000,
   delayMs: 500,
   message: 'You have reached your limit of requests',
 });
